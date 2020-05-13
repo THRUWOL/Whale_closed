903b553b4a1b4877a829b484ebf36ee8dc5e5e4c
cyberThruwol <therussianwolf@bk.ru>
2020-05-13 19:02:21 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,20 +2,23 @@ import discord
 import os
 from discord.ext import commands
 from discord.utils import get
-import youtube_dl
 
-
+    # Присваивание префикса командам
 bot = commands.Bot(command_prefix='#')
 
+    # Проверка работоспособности в консоли
 @bot.event
 async def on_ready():
-    print('+')
+    print('[log]буль')
 
-
+    # Проверка работоспособности
 @bot.command(pass_context=True)
 async def bot(ctx, amount = 1):
     await ctx.channel.purge(limit = amount)
     await ctx.send('буль')
 
+    # Токен, который находится подальше от чужих глаз
 token = os.environ.get('BOT_TOKEN')
+
+    # Запуск бота
 bot.run(token)
