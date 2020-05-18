a54a5c2c6c091680404098d542d9672a825f0839
THRUWOL <thruwol@yandex.ru>
2020-05-18 11:09:11 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,13 +31,13 @@ class MyClient(discord.Client):
 
                 if(len([i for i in member.roles if i.id not in config.EXCROLES]) <= config.MAX_ROLES_PER_USER):
                     await member.add_roles(role)
-                    print('[SUCCESS] Пользователь {0.display_name} получил роль {1.name}'.format(member, role))
+                    print('[SUCCESS]  {0.display_name} has been granted with role {1.name}'.format(member, role))
                 else:
                     await message.remove_reaction(payload.emoji, member)
-                    print('[ERROR] Слишком много ролей у пользователя {0.display_name}'.format(member))
+                    print('[ERROR] Too many roles for user {0.display_name}'.format(member))
 
             except KeyError as e:
-                print('[ERROR] Такой роли не найдено ' + emoji)
+                print('[ERROR] KeyError, no role found for ' + emoji)
             except Exception as e:
                 print(repr(e))
 
@@ -52,10 +52,10 @@ class MyClient(discord.Client):
             role = utils.get(message.guild.roles, id=config.ROLES[emoji]) # объект выбранной роли (если есть)
 
             await member.remove_roles(role)
-            print('[SUCCESS] Пользователь {1.name} убрал роль {0.display_name}'.format(member, role))
+            print('[SUCCESS] Role {1.name} has been remove for user {0.display_name}'.format(member, role))
 
         except KeyError as e:
-            print('[ERROR] Такой роли не найдено ' + emoji)
+            print('[ERROR] KeyError, no role found for ' + emoji)
         except Exception as e:
             print(repr(e))
 
