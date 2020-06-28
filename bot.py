2e8875cd91acd86827bfa8c969191453d6dd7579
THRUWOL <thruwol@yandex.ru>
2020-06-28 15:41:20 +0300

    card

--- bot.py
+++ bot.py
@@ -7,6 +7,9 @@ by Nikita [thruwol] Yarosh
 
 import discord
 import os
+import requests
+import io
+from PIL import Image, ImageFont, ImageDraw
 from discord.ext import commands
 
 client = commands.Bot(command_prefix = '.')
@@ -85,6 +88,32 @@ async def on_raw_reaction_remove(payload):
                 print("Member not found")
         else:
             print("Role not found")
+
+@client.command(aliases = ['я', 'карта'])
+async def card_user(ctx):
+    img = Image.new('RGBA',(400, 200), '#232529')
+    url = str(ctx.author.avatar_url)[:-10]
+
+    response = requests.get(url, stream = True)
+    response = Image.open(io.BytesIO(response.content))
+    response = response.convert('RGBA')
+    response = response.resize((100, 100), Image.ANTIALIAS)
+
+    img.paste(response, (15, 15, 115, 115))
+    idraw = ImageDraw.Draw(img)
+    name = ctx.author.name
+    tag = ctx.author.discriminator
+
+    headline = ImageFont.truetype('arial.ttf', size = 20)
+    undertext = ImageFont.truetype('arial.ttf', size = 12)
+
+    idraw.text((145, 15), f'{name}#{tag}', font = headline)
+    idraw.text((145, 50), f'ID: {ctx.author.id}', font = undertext)
+
+    img.save('user_card.png')
+
+    await ctx.send(file = discord.File(fp = 'user_card.png'))
+
 
 # Запуск бота
 client.run(os.environ.get('BOT_TOKEN'))
