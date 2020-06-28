cbb705c128b2d1514fa56d3547dab0ee2b36da8f
THRUWOL <thruwol@yandex.ru>
2020-06-28 15:56:24 +0300

    d

--- bot.py
+++ bot.py
@@ -7,12 +7,10 @@ by Nikita [thruwol] Yarosh
 
 import discord
 import os
-import requests
-import io
-from PIL import Image, ImageFont, ImageDraw
 from discord.ext import commands
 
 client = commands.Bot(command_prefix = '.')
+ID = 715454105706823731;
 
 # Проверка работоспособности
 @client.event
@@ -23,7 +21,7 @@ async def on_ready():
 @client.event
 async def on_raw_reaction_add(payload):
     message_id = payload.message_id
-    if message_id == 715454105706823731:
+    if message_id == ID:
         guild_id = payload.guild_id
         guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
 
@@ -58,7 +56,7 @@ async def on_raw_reaction_add(payload):
 @client.event
 async def on_raw_reaction_remove(payload):
     message_id = payload.message_id
-    if message_id == 715454105706823731:
+    if message_id == ID:
         guild_id = payload.guild_id
         guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
 
@@ -88,32 +86,6 @@ async def on_raw_reaction_remove(payload):
                 print("Member not found")
         else:
             print("Role not found")
-
-@client.command(aliases = ['я', 'карта'])
-async def card_user(ctx):
-    img = Image.new('RGBA',(400, 200), '#232529')
-    url = str(ctx.author.avatar_url)[:-10]
-
-    response = requests.get(url, stream = True)
-    response = Image.open(io.BytesIO(response.content))
-    response = response.convert('RGBA')
-    response = response.resize((100, 100), Image.ANTIALIAS)
-
-    img.paste(response, (15, 15, 115, 115))
-    idraw = ImageDraw.Draw(img)
-    name = ctx.author.name
-    tag = ctx.author.discriminator
-
-    headline = ImageFont.truetype('arial.ttf', size = 20)
-    undertext = ImageFont.truetype('arial.ttf', size = 12)
-
-    idraw.text((145, 15), f'{name}#{tag}', font = headline)
-    idraw.text((145, 50), f'ID: {ctx.author.id}', font = undertext)
-
-    img.save('user_card.png')
-
-    await ctx.send(file = discord.File(fp = 'user_card.png'))
-
 
 # Запуск бота
 client.run(os.environ.get('BOT_TOKEN'))
